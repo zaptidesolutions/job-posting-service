05302a58b9f361c907714da1ea45c1de70ccc27d
zaptidesolutions <founder.zaptide.solutions@gmail.com>
2025-10-09 02:51:35 +0530

    Apply API v1

--- src/controllers/v1/job_applying_controller.py
+++ src/controllers/v1/job_applying_controller.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, HTTPException, Body,status
+from fastapi import APIRouter, Body,status
 from models.user_applied_job import UserAppliedJob
-from bson import ObjectId
-from config.db_config import freelance_db as db
+from service.job_applying_service import apply_to_job
 
 router = APIRouter()
 
 # ---------------- Job Applying Endpoints ----------------
 @router.post("/v1/jobs/apply", status_code=status.HTTP_201_CREATED)
 async def apply_to_job(application: UserAppliedJob = Body(...)):
+    await apply_to_job(application)
